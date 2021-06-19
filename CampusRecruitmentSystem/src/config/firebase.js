8c2063a7a2bb73a041b94fefbf488d6fc2e5574e
saleempirwani <salimraza3465@gmail.com>
2021-06-19 17:39:30 +0500

    update firebase config

--- CampusRecruitmentSystem/src/config/firebase.js
+++ CampusRecruitmentSystem/src/config/firebase.js
@@ -1,12 +1,7 @@
 import * as Firebase from 'firebase';
 
-var config = {
-  apiKey: 'AIzaSyBmuwxe6I4MYIJRqiW6z0_vujq9yu9eE6Y',
-  authDomain: 'campus-recruitment-syste-47c65.firebaseapp.com',
-  projectId: 'campus-recruitment-syste-47c65',
-  storageBucket: 'campus-recruitment-syste-47c65.appspot.com',
-  messagingSenderId: '1033355349829',
-  appId: '1:1033355349829:web:067d2da2123fb3d4aab82c',
+const config = {
+  //  add your firebase configuration here...
 };
 // Initialize Firebase
 
